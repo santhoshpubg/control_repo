852e8c693848ce3cc22f86173f43c1450208d31d
santhoshpubg <56072733+santhoshpubg@users.noreply.github.com>
2020-02-05 00:03:14 +0000

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -6,10 +6,10 @@ class profile::ssh_server {
                 ensure => 'running',
                 enable => 'true',
       }
-      ssh_authorized_key {'root@master.puppet.vm':
+      ssh_authorized_key { 'root@master.puppet.vm':
                 ensure => present,
                 user => 'root',
                 type => 'ssh-rsa',
-                key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCpYYtUdVcINuPwCHYje/lzuuPYewBDHoJp/dBY65+yLqBed4TuGvtLkViVzMQeB0lPrZde5f3kaQd9ORQhprK8kHxng+ww7iHH+MBg5aSdJd0WQnNsiplKLwRLcG9QvbTPY8C2UzjSbfJ8wAgSHVf/uF/glMESkcCSSCOmDwGdn1738IUIVWh7dFn9+XUsd8S8SQdRHielMZg2Itknf4main5MCN3z+gPt+vWt6hFjEZ57BmfG2cATSuONtHlZFLwWsdA6jwAHJszdtQZAqk8y2FbbZR5wNyQ/AypiviSyPCvyPSVnNN65I6ECTEyl2GClYSlit6HiTWWZX6rR3',
+                key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCpYYtUdVcINuPwCHYje/lzuuPYewBDHoJp/dBY65+yLqBed4TuGvtLkViVzMQeB0lPrZde5f3kaQd9ORQhprK8kHxng+ww7iHH+MBg5aSdJd0WQnNsiplKLwRLcG9QvbTPY8C2UzjSbfJ8wAgSHVf/uF/glMESkcCSSCOmDwGdn1738IUIVWh7dFn9+XUsd8S8SQdRHielMZg2Itknf4main5MCN3z+gPt+vWt6hFjEZ57BmfG2cATSuONtHlZFLwWsdA6jwAHJszdtQZAqk8y2FbbZR5wNyQ/AypiviSyPCvyPSVnNN65I6ECTEyl2GClYSlit6HiTWWZX6rR3' ,
       }
 }
